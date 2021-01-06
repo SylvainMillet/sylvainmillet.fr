8a8410069e81eb24ae6bb47742ab5d8e28e86594
Sylvain Millet <kira69007@gmail.com>
2021-01-06 22:57:58 +0100

    first commit JS brouillon

---
+++ analyse.js
@@ -0,0 +1,8 @@
+function controle(form1) {
+    var name = document.form1.name.value;
+    alert("Vous avez tapé : " + test);
+    }
+
+
+var standard = "Joyeux xxème anniversaire en 2021";
+var Marc = 'Volvo XC60';
